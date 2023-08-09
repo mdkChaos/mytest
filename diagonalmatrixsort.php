f108e254550ce233a360dd1f660145a6ac2e7aa8
Andrii Poludniev <mdkchaos1@gmail.com>
2023-08-09 21:49:21 +0200

    fix

--- diagonalmatrixsort.php
+++ diagonalmatrixsort.php
@@ -1,22 +1,23 @@
 <?php
-function sortDiagonal($matrix) {
+function sortDiagonal($matrix)
+{
     $diagonal = array(); // Масив для збереження елементів бічної діагоналі
-    
+
     $size = count($matrix);
-    
+
     // Витягуємо елементи бічної діагоналі
     for ($i = 0; $i < $size; $i++) {
         $diagonal[] = $matrix[$i][$size - $i - 1];
     }
-    
+
     // Сортуємо елементи бічної діагоналі
     sort($diagonal);
-    
+
     // Підставляємо відсортовані елементи назад у матрицю
     for ($i = 0; $i < $size; $i++) {
         $matrix[$i][$size - $i - 1] = array_pop($diagonal);
     }
-    
+
     return $matrix;
 }
 
