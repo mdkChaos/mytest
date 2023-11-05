c1c20a32eef6cdbff7a31d9a7b8a97ede3b2e2b8
Andrii Poludniev <mdkchaos1@gmail.com>
2023-11-05 19:26:21 +0100

    a

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <?php
-    $flag = true;
-    $iteration = 100000000;
+    
+    function isPrime($number) {
+        if ($number <= 1) {
+            return false;
+        }
+        
+        // Перевірка дільників з 2 до квадратного кореня з числа
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            if ($number % $i == 0) {
+                return false; // Знайдено дільник, число не є простим
+            }
+        }
+        
+        return true; // Число є простим
+    }
+    
+    $lower_limit = 2;
+    $upper_limit = 100000;
+    $nums = [];
+    
     $start_time = hrtime(true);
 
     // Ваш код або функція, яку ви хочете виміряти
-    for ($i=0; $i < $iteration; $i++) { 
-        unset($flag);
+    for ($num = $lower_limit; $num <= $upper_limit; $num++) {
+        if (isPrime($num)) {
+            array_push($nums, $num);
+        }
     }
     
     $end_time = hrtime(true);
     $execution_time_ns = $end_time - $start_time;
     $execution_time_s = $execution_time_ns / 1e9; // перетворення наносекунд у секунди
-    
+    echo "<br>";
     echo "Час виконання: " . $execution_time_s . " секунд";
     echo "<br>";
-
-    $flag = true;
-    $start_time = hrtime(true);
-
-    // Ваш код або функція, яку ви хочете виміряти
-    for ($i=0; $i < $iteration; $i++) { 
-        $flag = null;
+    echo "Прості числа в діапазоні від $lower_limit до $upper_limit:";
+    echo "<br>";
+    foreach($nums as $num)
+    {
+        echo "$num ";
     }
-    
-    $end_time = hrtime(true);
-    $execution_time_ns = $end_time - $start_time;
-    $execution_time_s = $execution_time_ns / 1e9; // перетворення наносекунд у секунди
-    
-    echo "Час виконання: " . $execution_time_s . " секунд";
-    echo "<br>";
